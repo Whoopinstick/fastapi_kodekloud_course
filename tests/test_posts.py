cb216ac8c2c7a81aa63f4d5d40fe893e19a19e03
Kevin Paul <k.m.paul68@gmail.com>
2025-08-25 22:00:14 -0400

    test get 1 post

--- tests/test_posts.py
+++ tests/test_posts.py
@@ -14,3 +14,25 @@ def test_get_all_posts(authorized_client, test_posts):
     print(posts_map_list)
 
     assert response.status_code == 200
+
+
+def test_get_one_post(authorized_client, test_posts):
+    response = authorized_client.get("/posts/1")
+    response_data = response.json()
+    print(response_data)
+    assert response.status_code == 200
+    assert response_data["title"] == "first title"
+
+
+def test_get_one_post_not_exist(authorized_client, test_posts):
+    response = authorized_client.get("/posts/1000")
+    response_data = response.json()
+    print(response.status_code)
+    assert response.status_code == 404
+
+
+def test_unauthorized_delete_post(client, test_posts):
+    response = client.delete("/posts/1")
+    # print(f"the response {response.json()}")
+    # print(response.status_code)
+    assert response.status_code == 401
